7ff227263cc0deb01bf6914de3a553dc1296617f
fskinner-dev <fskinner@yahoo.com>
2024-07-20 10:33:37 -0400

    Pigeon pea page layout updates

--- js/script.js
+++ js/script.js
@@ -56,10 +56,10 @@
 		}
 		tablinks = document.getElementsByClassName("tablink");
 		for (i = 0; i < x.length; i++) {
-			tablinks[i].className = tablinks[i].className.replace("w3-dark-gray", "");
+			tablinks[i].className = tablinks[i].className.replace("w3-white", "");
 		}
 		document.getElementById(menuName).style.display = "block";
-		evt.currentTarget.firstElementChild.className += " w3-black";
+		evt.currentTarget.firstElementChild.className += " w3-white";
 
 		switch (menuName) {
 			case 'DRUM FOR LIFE':
